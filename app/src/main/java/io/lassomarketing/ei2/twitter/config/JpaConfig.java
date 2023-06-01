a9381f64181f7488f13bf35a2597d00cc0dd27d4
Roman Rasskazov <romanr@lassomarketing.io>
2023-06-01 17:32:47 +0300

    EI2-278 - connect database

--- app/src/main/java/io/lassomarketing/ei2/twitter/config/JpaConfig.java
+++ app/src/main/java/io/lassomarketing/ei2/twitter/config/JpaConfig.java
@@ -1,9 +1,50 @@
 package io.lassomarketing.ei2.twitter.config;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zaxxer.hikari.HikariConfig;
+import io.lassomarketing.ei2.config.AppDBConfig;
+import io.lassomarketing.ei2.config.DatasourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
 public class JpaConfig {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Bean
+    public JsonNode twitterDbSecret(@Value("${sm://ei2-twitter-service-db-secret}") String secretValue) throws Exception {
+        try {
+            return objectMapper.readTree(secretValue);
+        } catch (JsonProcessingException e) {
+            throw new Exception("Secret ei2-twitter-service-db-secret contains invalid json", e);
+        }
+    }
+
+    @Bean
+    @ConfigurationProperties("app.db.twitter")
+    public AppDBConfig dbConfig(JsonNode camundaDbSecret) {
+        return new AppDBConfig(camundaDbSecret);
+    }
+
+    @Bean
+    @Primary
+    public DataSource camundaDataSource(HikariConfig appPostgresHikariConfig, AppDBConfig dbConfig) {
+        DatasourceBuilder dsBuilder = new DatasourceBuilder(dbConfig, appPostgresHikariConfig);
+
+        return dsBuilder.build();
+    }
+
 }
